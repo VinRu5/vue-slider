91ad1c9f50a357cf4590ca2092df12291a2df0a1
VinRu5 <vincenzorusso.inbox@gmail.com>
2021-06-25 16:03:37 +0200

    css dot

--- js/script.js
+++ js/script.js
@@ -5,25 +5,35 @@ new Vue({
     data: {
         photos: [
             './img/images-1.jpg',
-            './img/images-2.jpg',
+            './img/images-8.jpg',
             './img/images-3.jpg',
             './img/images-4.jpg',
             './img/images-7.jpg',
         ],
         imgIndex: 0,
     },
+    created(){
+        setInterval(() => {
+            this.next();
+        }, 5000);
+    },
     methods: {
         next: function () {
             if (this.imgIndex === (this.photos.length - 1)) {
                 this.imgIndex = 0;
+            } else {
+                this.imgIndex++;
             }
-            this.imgIndex++;
         },
         back: function () {
             if (this.imgIndex === 0) {
-                this.imgIndex = this.photos.length;
+                this.imgIndex = this.photos.length - 1;
+            } else {
+                this.imgIndex--;
             }
-            this.imgIndex--;
+        },
+        detected: function (index) {
+            
         }
     }
 })

692181b95d842073cef1b3db5f2f92c858e959e1
VinRu5 <vincenzorusso.inbox@gmail.com>
2021-06-25 15:36:16 +0200

    struttura base e freccie

--- js/script.js
+++ js/script.js
@@ -3,9 +3,27 @@ Vue.config.devtools = true;
 new Vue({
     el: '#app',
     data: {
-        
+        photos: [
+            './img/images-1.jpg',
+            './img/images-2.jpg',
+            './img/images-3.jpg',
+            './img/images-4.jpg',
+            './img/images-7.jpg',
+        ],
+        imgIndex: 0,
     },
     methods: {
-
+        next: function () {
+            if (this.imgIndex === (this.photos.length - 1)) {
+                this.imgIndex = 0;
+            }
+            this.imgIndex++;
+        },
+        back: function () {
+            if (this.imgIndex === 0) {
+                this.imgIndex = this.photos.length;
+            }
+            this.imgIndex--;
+        }
     }
 })

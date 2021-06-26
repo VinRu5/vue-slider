67660eef548322158ba413e15adf2ab1c063ed7d
VinRu5 <vincenzorusso.inbox@gmail.com>
2021-06-26 15:58:20 +0200

    sistemazione rotate

--- js/script.js
+++ js/script.js
@@ -5,18 +5,15 @@ new Vue({
     data: {
         photos: [
             './img/images-1.jpg',
-            './img/images-8.jpg',
+            './img/images-2.jpg',
             './img/images-3.jpg',
             './img/images-4.jpg',
+            './img/images-5.jpg',
+            './img/images-6.jpeg',
             './img/images-7.jpg',
+            './img/images-8.jpg'
         ],
-        photos2: [
-            '../img/images-1.jpg',
-            '../img/images-8.jpg',
-            '../img/images-3.jpg',
-            '../img/images-4.jpg',
-            '../img/images-7.jpg',
-        ],
+
         imgIndex: 0,
         intervallSelected: null,
         active: ''
@@ -51,12 +48,21 @@ new Vue({
         },
         noRotate: function () {
             clearInterval(this.intervallSelected);
-            //sitemare qui
         },
         rotate: function () {
             this.intervallSelected = setInterval(() => {
                 this.next();
             }, 5000);
+        },
+        noRotateButton: function () {
+            if (this.active === 'no-rotate') {
+                this.rotate();
+                this.active = '';
+            } else {
+                this.noRotate();
+                this.active = 'no-rotate';
+            }
+
         }
     }
 })
